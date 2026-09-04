4d7218ab95153f31be743d5ee1d2782fd634e855
Arash Rahmani Dehkordi <rdehkordi.arash@gmail.com>
2026-09-03 20:30:25 -0400

    Update script.js

--- personal/script.js
+++ personal/script.js
@@ -1,1 +1,37 @@
 
+const buttons = document.querySelectorAll(".nav-button");
+
+const sections = document.querySelectorAll(".content-section");
+
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const sectionId = this.dataset.section;
+
+
+        // Hide all sections
+        sections.forEach(section => {
+            section.classList.remove("active");
+        });
+
+
+        // Remove active style from all buttons
+        buttons.forEach(button => {
+            button.classList.remove("active");
+        });
+
+
+        // Show selected section
+        document
+            .getElementById(sectionId)
+            .classList.add("active");
+
+
+        // Highlight selected button
+        this.classList.add("active");
+
+    });
+
+});
